f15ac170de87e5085bb84bef2f5cdf777f931466
uddeepk <uddeepk@gmail.com>
2021-05-01 19:35:49 -0800

    add ctor getter setters to person class

--- people/Person.cpp
+++ people/Person.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Person.hpp"
+
+Person::Person(const std::string& name, const std::string& gender): _name(name), _gender(_gender) {
+
+}
+
+void Person::setName(const std::string &newName) {
+    _name = newName;
+}
+
+std::string Person::getName() const {
+    return _name;
+}
+
+void Person::setGender(const std::string &newGender) {
+    _gender = newGender;
+}
+
+std::string Person::getGender() const {
+    return _gender;
+}
--- people/Person.hpp
+++ people/Person.hpp
@@ -4,9 +4,22 @@
 
 #ifndef PEOPLE_PERSON_HPP
 #define PEOPLE_PERSON_HPP
-
+#include <string>
 
 class Person {
+public:
+    // Constructor
+    Person(const std::string& name, const std::string& gender);
+
+    // Getters and Setters
+    void setName(const std::string &newName);
+    std::string getName () const;
+    void setGender(const std::string &newGender);
+    std::string getGender() const;
+
+private:
+    std::string _name;
+    std::string _gender;
 
 };
 
