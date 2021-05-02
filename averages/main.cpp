831edf712d62db5ef30637376b37303c1871945b
uddeepk <uddeepk@gmail.com>
2021-05-01 18:10:43 -0800

    tried product of scores using std::multiples

--- averages/main.cpp
+++ averages/main.cpp
@@ -12,20 +12,30 @@
 #include <numeric>
 
 double average_score(const std::vector<int> &scores);
+double scores_product(const std::vector<int> &scores);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     // creating test vector
-    std::vector<int> v(10);
+    std::vector<int> v(5);
     std::iota(v.begin(), v.end(), 1);
-    std::cout << average_score(v);
+    std::cout << average_score(v) << "\n";
+    std::cout << scores_product(v) << "\n";
     return 0;
 }
 
-double average_score(const std::vector<int> &scores) {
+double average_score(const std::vector<int>& scores) {
     //return std::accu
     return std::accumulate(
-            scores.begin(), scores.end()
-            , 0
+            scores.cbegin(), scores.cend(),
+            0
             ) / static_cast <double> (scores.size());
+}
+
+double scores_product(const std::vector<int>& scores) {
+    return std::accumulate(
+            scores.cbegin(), scores.cend(),
+            1,
+            std::multiplies()
+            );
 }
