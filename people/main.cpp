22e0d6eea208077671c397d5052b64daec600648
uddeepk <uddeepk@gmail.com>
2021-05-01 20:16:08 -0800

    made function getPeople to return vector or Persons

--- people/main.cpp
+++ people/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+#include "Person.hpp"
+
+using std::vector;
+using std::string;
+
+vector <Person> vecPeople () ;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+
+//    vector <Person> people ;
+    //Person p1 {"ss", "gg"};
+    auto people = vecPeople();
     return 0;
+}
+
+vector <Person> vecPeople() {
+    vector <vector <string>> people = {
+            {"Paul",     "male"},
+            {"Veronica", "female"},
+            {"Robert",   "male"},
+            {"Theresa",   "female"},
+            {"Theodore", "male"},
+            {"Angie",    "female"}
+    };
+    vector <Person> v;
+
+    for ( const auto &p : people) {
+        v.push_back(Person(p[0], p[1]));
+    }
+
+    return v;
 }
