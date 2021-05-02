e502d2705e9a85e036e3784f1b99ea0cfd03c2d8
uddeepk <uddeepk@gmail.com>
2021-05-01 18:18:23 -0800

    std::accumulate to count newlines

--- averages/main.cpp
+++ averages/main.cpp
@@ -13,6 +13,8 @@
 
 double average_score(const std::vector<int> &scores);
 double scores_product(const std::vector<int> &scores);
+int count_lines(const std::string& s);
+int f(int previous_count, char c);
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -21,6 +23,7 @@ int main() {
     std::iota(v.begin(), v.end(), 1);
     std::cout << average_score(v) << "\n";
     std::cout << scores_product(v) << "\n";
+    std::cout << count_lines("h\ns\ntest\n4\n") << "\n";
     return 0;
 }
 
@@ -38,4 +41,17 @@ double scores_product(const std::vector<int>& scores) {
             1,
             std::multiplies()
             );
+}
+
+// Counting newlines using std::accumulate
+int count_lines(const std::string &s) {
+    return std::accumulate(
+            s.cbegin(), s.cend(),
+            0,
+            f
+            );
+}
+
+int f(int previous_count, char c) { // (int, char) -> int
+    return (c != '\n' ? previous_count : previous_count + 1);
 }
